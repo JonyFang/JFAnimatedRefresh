da0f9e95290cc928e5eaf8f9dc3ac5cf691cab22
JonyFang <ahchunfang@gmail.com>
2017-09-11 17:45:39 +0800

    Init project

---
+++ JFAnimatedRefreshExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JFAnimatedRefreshExample
+//
+//  Created by JonyFang on 2017/9/11.
+//  Copyright © 2017年 JonyFang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
